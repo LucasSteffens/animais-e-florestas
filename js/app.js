09fe12fd42566bc521849f17a8339a563d9386d3
Lucas Boesing Steffens <lucas.steffens2012@hotmail.com>
2023-06-12 08:32:05 -0300

    add animation to animals

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var classActive = 'active';
+
+$('.tab-menu a').first().addClass(classActive);
+$('.item').first().addClass(classActive);
+
+$('.tab-menu a').click(function(e){
+    e.preventDefault();
+    var itemId = $(this).attr('href');
+
+    $('.tab-menu a, .item').removeClass(classActive);
+    $(this).addClass(classActive);
+    $(itemId).addClass(classActive);
+});
