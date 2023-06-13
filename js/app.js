a894767c0be87a3a0dc69803fad9893d9360d9d6
Lucas Boesing Steffens <lucas.steffens2012@hotmail.com>
2023-06-13 07:46:18 -0300

    add click targeting

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
-var classActive = 'active';
+$(document).ready(function(){
+    var classActive = 'active';
 
-$('.tab-menu a').first().addClass(classActive);
-$('.item').first().addClass(classActive);
-
-$('.tab-menu a').click(function(e){
-    e.preventDefault();
-    var itemId = $(this).attr('href');
-
-    $('.tab-menu a, .item').removeClass(classActive);
-    $(this).addClass(classActive);
-    $(itemId).addClass(classActive);
-});
+    $('.animais .tab-menu a').first().addClass(classActive);
+    $('.animais .item').first().addClass(classActive);
+    
+    $('.animais .tab-menu a').click(function(e){
+        e.preventDefault();
+        var itemId = $(this).attr('href');
+    
+        $('.animais .tab-menu a, .animais .item').removeClass(classActive);
+        $(this).addClass(classActive);
+        $(itemId).addClass(classActive);
+    });
+    
+    $('.florestas .tab-menu a').first().addClass(classActive);
+    $('.florestas .item').first().addClass(classActive);
+    
+    $('.florestas .tab-menu a').click(function(e){
+        e.preventDefault();
+        var itemId = $(this).attr('href');
+    
+        $('.florestas .tab-menu a, .florestas .item').removeClass(classActive);
+        $(this).addClass(classActive);
+        $(itemId).addClass(classActive);
+    });
+})
