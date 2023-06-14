24df23e8ed67495af803808daa9e3121cc7242cc
Lucas Boesing Steffens <lucas.steffens2012@hotmail.com>
2023-06-13 21:18:52 -0300

    Add click targeting - optimized

--- js/app.js
+++ js/app.js
@@ -1,27 +1,50 @@
-$(document).ready(function(){
-    var classActive = 'active';
+// $(document).ready(function(){
+//     var classActive = 'active';
 
-    $('.animais .tab-menu a').first().addClass(classActive);
-    $('.animais .item').first().addClass(classActive);
+//     $('.animais .tab-menu a').first().addClass(classActive);
+//     $('.animais .item').first().addClass(classActive);
     
-    $('.animais .tab-menu a').click(function(e){
+//     $('.animais .tab-menu a').click(function(e){
+//         e.preventDefault();
+//         var itemId = $(this).attr('href');
+    
+//         $('.animais .tab-menu a, .animais .item').removeClass(classActive);
+//         $(this).addClass(classActive);
+//         $(itemId).addClass(classActive);
+//     });
+    
+//     $('.florestas .tab-menu a').first().addClass(classActive);
+//     $('.florestas .item').first().addClass(classActive);
+    
+//     $('.florestas .tab-menu a').click(function(e){
+//         e.preventDefault();
+//         var itemId = $(this).attr('href');
+    
+//         $('.florestas .tab-menu a, .florestas .item').removeClass(classActive);
+//         $(this).addClass(classActive);
+//         $(itemId).addClass(classActive);
+//     });
+// })
+// Codigo Otimizado abaixo:
+
+$('[data-group]').each(function(){
+    var allTarget = $(this).find('[data-target]'),
+        allClick = $(this).find('[data-click]'),
+        activeClass = 'active';
+
+    allTarget.first().addClass(activeClass);
+    allClick.first().addClass(activeClass);
+
+    allClick.click(function(e){
         e.preventDefault();
-        var itemId = $(this).attr('href');
-    
-        $('.animais .tab-menu a, .animais .item').removeClass(classActive);
-        $(this).addClass(classActive);
-        $(itemId).addClass(classActive);
+
+        var id = $(this).data('click'),
+            target = $('[data-target="' + id + '"]');
+
+        allClick.removeClass(activeClass);
+        allTarget.removeClass(activeClass);
+
+        target.addClass(activeClass);
+        $(this).addClass(activeClass);
     });
-    
-    $('.florestas .tab-menu a').first().addClass(classActive);
-    $('.florestas .item').first().addClass(classActive);
-    
-    $('.florestas .tab-menu a').click(function(e){
-        e.preventDefault();
-        var itemId = $(this).attr('href');
-    
-        $('.florestas .tab-menu a, .florestas .item').removeClass(classActive);
-        $(this).addClass(classActive);
-        $(itemId).addClass(classActive);
-    });
-})
+});
